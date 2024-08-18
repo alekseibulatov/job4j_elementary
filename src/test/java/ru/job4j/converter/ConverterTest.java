a0d1b237c3459ffb5dba365fc4ba5457ffe3cdae
alekseibulatov <bulatov2803@gmail.com>
2024-08-19 01:11:14 +0300

    1. Модульные тесты [#505277]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,62 @@
+package ru.job4j.converter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ConverterTest {
+
+    @Test
+    void whenConvert180RblThen2Euro() {
+        double input = 180;
+        double expected = 2;
+        double output = Converter.rubleToEuro(input);
+        double value = 0.0001;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @Test
+    void whenConvert200RblThen2dot2222Euro() {
+        double input = 200;
+        double expected = 2.2222;
+        double output = Converter.rubleToEuro(input);
+        double value = 0.0001;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @Test
+    void whenConvert0RblThen0Euro() {
+        double input = 0;
+        double expected = 0;
+        double output = Converter.rubleToEuro(input);
+        double value = 0.0001;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @Test
+    void whenConvert225RblThen3Dollars() {
+        double input = 225;
+        double expected = 3;
+        double output = Converter.rubleToDollar(input);
+        double value = 0.0001;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @Test
+    void whenConvert0RblThen0Dollars() {
+        double input = 0;
+        double expected = 0;
+        double output = Converter.rubleToDollar(input);
+        double value = 0.0001;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @Test
+    void whenConvert75RblThen1Dollars() {
+        double input = 75;
+        double expected = 1;
+        double output = Converter.rubleToDollar(input);
+        double value = 0.0001;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+}
