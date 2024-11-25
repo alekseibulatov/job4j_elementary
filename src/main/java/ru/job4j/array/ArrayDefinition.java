98391bf1d904bf275ed04cd87cddaa7dd7b4b8c2
alekseibulatov <bulatov2803@gmail.com>
2024-11-25 22:30:23 +0300

    Заполнение массива

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,8 +5,17 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-        System.out.println(String.format("Size array ages is: %s", ages.length));
-        System.out.println(String.format("Size array surnames is: %s", surnames.length));
-        System.out.println(String.format("Size array prices is: %s", prices.length));
+        String[] names = new String[4];
+        names[0] = "Alex";
+        names[1] = "Andrey";
+        names[2] = "Misha";
+        names[3] = "Den";
+        System.out.printf("Name first: %s%n", names[0]);
+        System.out.printf("Name second: %s%n", names[1]);
+        System.out.printf("Name third: %s%n", names[2]);
+        System.out.printf("Name four: %s%n", names[3]);
+        System.out.printf("Size array ages is: %s%n", ages.length);
+        System.out.printf("Size array surnames is: %s%n", surnames.length);
+        System.out.printf("Size array prices is: %s%n", prices.length);
     }
 }
